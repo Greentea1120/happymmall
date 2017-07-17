d3f24907f0c24a3b623c9e798c4b15787eaf9dbf
WangYukun <wangyukun@wangyukundeAir.lan>
2017-07-18 04:22:26 +0800

    7.18 init

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var htmlWebpackPlugin = require('html-webpack-plugin');     //1.引入
+
+//获取html-webpack-plugin获取参数的方法
+var getHtmlConfig = function(name){
+    return {
+        template:'./src/view/' + name +'.html',
+        filename:'view/' + name + '.html',
+        inject:true,
+        hash:true,
+        chunks:['common',name]
+    }
+}
+
 var config = {
     entry: {
         'common':['./src/page/common/index.js'],
@@ -7,22 +21,31 @@ var config = {
     },
     output: {
         path: './dist',
-        filename: '/js/[name].js'
+        filename: 'js/[name].js'
     },
     externals:{
         'jquery':'window.jQuery'
     },
-    module:{
-      loaders:[
-          {text:/\.css$/,loader:"style-loader!css-loader"}   //从右往左,先执行css-loader再执行style-loader
-      ]
-    },
+module:{
+  loaders:[
+      {test:/\.css$/,loader:ExtractTextPlugin.extract("style-loader","css-loader")},
+      {test:/\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/,loader:'file-loader?limit=1000&name=resource/[name].[ext]'}
+
+  ]
+},
     plugins:[
+        //独立通用模块js/base.js
         new webpack.optimize.CommonsChunkPlugin({
-            name:'common',     //这个common和entry的common对应,否则common会生成common.js而不是base.js
+            name:'common',
             filename:'js/base.js'
-        })
+        }),
+        //把css单独打包到文件里
+        new ExtractTextPlugin("css/[name].css"),
+        //html模板的处理
+        new htmlWebpackPlugin(getHtmlConfig('index')),
+        new htmlWebpackPlugin(getHtmlConfig('login'))
     ]
 };
 
 module.exports = config
+
