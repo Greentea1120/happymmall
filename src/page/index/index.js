26cf6fa561e540a54e700288bd22a95805b51eeb
greentea <741024920@qq.com>
2017-07-20 18:00:42 +0800

    tools complete

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,13 +2,8 @@
 
 var _mm = require('util/mm.js');
 
-_mm.request({
-    url:'/product/list.do?keyword=1',
-    success:function (res) {
-        console.log(res)
-    },
-    error:function (errMsg) {
-        console.log(errMsg)
-    }
-})
-console.log(_mm.getUrlParam('abc'))
+var html = '<div>{{ data }}</div>';
+var data = {
+    data:123
+}
+console.log(_mm.renderHtml(html,data))
