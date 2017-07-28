2538cc7df30057e8aa87c6cc8aa407ce81837fa4
greentea <741024920@qq.com>
2017-07-28 15:25:50 +0800

    小改动

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ var config = {
     module:{
       loaders:[
           {test:/\.css$/,loader:ExtractTextPlugin.extract("style-loader","css-loader")},
-          {test:/\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/,loader:'file-loader?limit=1000&name=resource/[name].[ext]'},
+          {test:/\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/,loader:'url-loader?limit=1000&name=resource/[name].[ext]'},
           {test:/\.string$/,loader:'html-loader'}
 
       ]
