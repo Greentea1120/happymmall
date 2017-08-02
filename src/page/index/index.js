09b11dfa9aaa8c52cdd6ae80cace1b647b0a40f1
Greentea1120 <741024920@qq.com>
2017-08-02 22:20:26 +0800

    slider

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -12,13 +12,11 @@ $(function() {
     //这里是渲染banner的html
     var bannerHtml = _mm.renderHtml(templateBanner);
     $('.banner-con').html(bannerHtml);
-
     //初始化banner
     var $slider = $('.banner').unslider({
         dots : true
     });
-
-    //前一张和后一张操作
+    //前一张和后一张按钮操作
     $('.banner-con .banner-arrow').click(function () {
         var forward = $(this).hasClass('prev') ? 'prev' : 'next';
         $slider.data('unslider')[forward]();
